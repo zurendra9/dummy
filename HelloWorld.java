164c8578c9d84644f094b750806a3a651c3bd10d
Surendra Sharma <surendra.sharma@mindtickle.com>
2018-06-23 21:23:52 +0530

    hope changes gets reflected on remote repo

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,10 +2,17 @@ import java.util.*;
 
 public class HelloWorld
 {
+  static void fun()
+  {
+    int one = 123;
+    int two = 3456;
+    System.out.println(one+""+two);
+  }
   public static void main(String[] args)
   {
     System.out.println("Watch out !!");
     System.out.println("Watching !");
     System.out.println("oh ho");
+    HelloWorld.fun();
   }
 }
